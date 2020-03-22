6e0f5c9babc8ed67618bf47112ba37e7b6d4ba95
Madhuni96 <43632067+Madhuni96@users.noreply.github.com>
2020-03-22 11:26:50 +0530

    Update signin_user.js(just a console.log)

--- src/redux/USER/signin_user.js
+++ src/redux/USER/signin_user.js
@@ -1,84 +1,80 @@
-import Axios from 'axios';
+import Axios from "axios";
 
 const intial_state = {
-    loading: false,
-    user: {},
-    error: {}
-}
+  loading: false,
+  user: {},
+  error: {}
+};
 
 //Types
-const FETCH_LOADING = 'FETCH_LOADING';
-const FETCH_SUCCESS = 'FETCH_LOADING';
-const FETCH_ERROR = 'FETCH_ERROR';
-
-
+const FETCH_LOADING = "FETCH_LOADING";
+const FETCH_SUCCESS = "FETCH_LOADING";
+const FETCH_ERROR = "FETCH_ERROR";
 
 const fetchLoading = () => {
-    return {
-        type: FETCH_LOADING
-    }
-}
+  return {
+    type: FETCH_LOADING
+  };
+};
 
-const fetchSuccess = (user) => {
-    return {
-        type: FETCH_SUCCESS,
-        payload: user
-    }
-}
+const fetchSuccess = user => {
+  return {
+    type: FETCH_SUCCESS,
+    payload: user
+  };
+};
 
-const fetchError = (error) => {
-    return {
-        type: FETCH_ERROR,
-        payload: error
-    }
-}
+const fetchError = error => {
+  return {
+    type: FETCH_ERROR,
+    payload: error
+  };
+};
 
-
-export const signin_action = (payload) => {
-
-    return dispatch => {
-        dispatch(fetchLoading());
-        Axios({
-            method: 'POST',
-            url: process.env.REACT_APP_BASE_URL + '/admin/signin',
-            data: {
-                username: payload.username,
-                password: payload.password
-            },
-            headers: { 'api_key': process.env.REACT_APP_API_KEY }
-        })
-            .then(res => {
-                const result = res.data;
-                dispatch(fetchSuccess(result));
-
-            })
-            .catch(err => {
-                const error = err.response;
-                dispatch(fetchError(error.data));
-
-            });
-    }
-}
+export const signin_action = payload => {
+  return dispatch => {
+    dispatch(fetchLoading());
+    Axios({
+      method: "POST",
+      url: process.env.REACT_APP_BASE_URL + "/admin/signin",
+      data: {
+        username: payload.username,
+        password: payload.password
+      },
+      headers: { api_key: process.env.REACT_APP_API_KEY }
+    })
+      .then(res => {
+        const result = res.data;
+        dispatch(fetchSuccess(result));
+        console.log("Success Msg:", result);
+      })
+      .catch(err => {
+        const error = err.response;
+        dispatch(fetchError(error.data));
+      });
+  };
+};
 
 export const signin_reducer = (state = intial_state, action) => {
-    switch (action.type) {
-        case FETCH_LOADING: return {
-            ...state,
-            loading: true
-        }
-        case FETCH_SUCCESS: return {
-            ...state,
-            loading: false,
-            user: action.payload
-        }
-        case FETCH_ERROR: return {
-            ...state,
-            loading: false,
-            error: action.payload
-        }
-        default: return state
-    }
-}
-
-
-
+  switch (action.type) {
+    case FETCH_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        user: action.payload
+      };
+    case FETCH_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
